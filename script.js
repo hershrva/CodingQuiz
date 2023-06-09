99602c3d169e7aa1150b9e5101684b49fcf03212
hershRVA <herchenrother@gmail.com>
2023-06-08 23:06:24 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+var timerEl = doucument.getElementById('countdown');
+var mainEl = document.getElementById('main');
+var question =
+  'Some say the world will end in 🔥, Some say in ice. From what I’ve tasted of desire, I hold with those who favor fire. But if it had to perish twice, I think I know enough of hate. To say that for destruction ice, Is also great, And would suffice.';
+var answerOne = ;
+var answerTwo = ;
+var answerThree = 
+var answerFour= []
+
+// Timer that counts down from 5
+function countdown() {
+  var timeLeft = 75;
+
+  // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+  var timeInterval = setInterval(function () {
+    // As long as the `timeLeft` is greater than 1
+    if (timeLeft > 1) {
+      // Set the `textContent` of `timerEl` to show the remaining seconds
+      timerEl.textContent = 'Time: ' + timeLeft;
+      // Decrement `timeLeft` by 1
+      timeLeft--;
+    } else {
+      // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+      timerEl.textContent = '';
+      // Use `clearInterval()` to stop the timer
+      clearInterval(timeInterval);
+      // Call the `displayMessage()` function
+      displayMessage();
+    }
+  }, 1000);
+}
+
+function displayMessage() {
+    var wordCount = 0;
+  
+    // Uses the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    var msgInterval = setInterval(function () {
+      // If there are no more words left in the message
+      if (words[wordCount] === undefined) {
+        // Use `clearInterval()` to stop the timer
+        clearInterval(msgInterval);
+      } else {
+        // Display one word of the message
+        mainEl.textContent = words[wordCount];
+        wordCount++;
+      }
+    }, 1000);
+  }
+
+  countdown();
