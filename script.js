465fe8f7db8b4c3dfc8a2c5c5e19aef66ca97837
hershRVA <herchenrother@gmail.com>
2023-06-12 14:16:20 -0400

    changes to java

--- script.js
+++ script.js
@@ -1,11 +1,68 @@
 var timerEl = doucument.getElementById('countdown');
 var mainEl = document.getElementById('main');
-var question =
-  'Some say the world will end in 🔥, Some say in ice. From what I’ve tasted of desire, I hold with those who favor fire. But if it had to perish twice, I think I know enough of hate. To say that for destruction ice, Is also great, And would suffice.';
-var answerOne = ;
-var answerTwo = ;
-var answerThree = 
-var answerFour= []
+var question = '';
+var startBtn = document.querySelector('.start-btn');
+var quizBox = document.querySelector('.quiz-box');
+var finalScore = document.querySelector('.results');
+var 
+var questionInfo = [
+    {
+        question: "Which of the following is not one of the four cornerstones of computational thinking?",
+        optionA: "Decomposition",
+        optionB:"Pattern recognition",
+        optionC:"Abstraction",
+        optionD:"Completion",
+        answer:"Completion",
+    },
+    {
+        question:"What information does the <head> element contain?",
+        optionA:"Content the User can see",
+        optionB:"Metadata",
+        optionC:"Link to the HTML document",
+        optionD:"<h1>, <h2>, <h3> elements",
+        answer:"Metadata",
+    },
+    {
+        question:"Which of the following is not used to identify elements in a CSS style sheet?",
+        optionA:"* universal",
+        optionB:". class",
+        optionC:"# id",
+        optionD:"All of the above",
+        answer:"All of the above",
+    },
+    {
+        question:"Declarations in CSS contain which of the following important components?",
+        optionA:"Property and Value",
+        optionB:"Property and Element",
+        optionC:"Length and Value",
+        optionD:"Length and Element",
+        answer:"Property and Value",
+    },
+    {
+        question:"A single variable used to hold a group of data in JavaScript is known as:",
+        optionA:"A Function",
+        optionB:"A For Loop",
+        optionC:"An Array",
+        optionD:"A Property",
+        answer:"An Array",
+    },
+    {
+        question:"What loop uses the predictable pattern of indices to perform a task on all the items in an array by allowing a single code block to be executed over and over?",
+        optionA:"A While Loop",
+        optionB:"An If Else Loop",
+        optionC:"A Hula Loop",
+        optionD:"A For Loop",
+        answer:"A For Loop",
+    },
+    {
+        question:"jQuery uses which symbol at the beginning of its syntax?",
+        optionA:"&",
+        optionB:"*",
+        optionC:"^",
+        optionD:"$",
+        answer:"$",
+    }
+]
 
 // Timer that counts down from 5
 function countdown() {
